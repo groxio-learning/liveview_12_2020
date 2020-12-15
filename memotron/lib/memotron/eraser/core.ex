0e12a52eafecd3683c5be0153d3ddbabc0d7b31e
Rob Shepherd <rshepherd.tech@gmail.com>
2020-12-15 21:17:25 +0000

    flush out the erase function

--- memotron/lib/memotron/eraser/core.ex
+++ memotron/lib/memotron/eraser/core.ex
@@ -1,13 +1,35 @@
-def Memotron.Eraser.Core do
+defmodule Memotron.Eraser.Core do
   defstruct [:schedule, :text]
-  
+
   def new(text, steps) do
     %__MODULE__{}
   end
-  
-  def erase(eraser) do
+
+  def erase(%{schedule: []} = eraser) do
     eraser
   end
-  
-  def example, do: %{text: "abcde", schedule: [[1, 2, 3], [4, 5]]}
+
+  def erase(%{schedule: [replacements | schedule], text: text} = eraser) do
+    %{eraser | schedule: schedule, text: replace(text, replacements)}
+  end
+
+  defp replace(text, replacements) do
+    text
+    |> String.graphemes()
+    |> Enum.with_index(1)
+    |> Enum.map(fn {char, i} ->
+      replace_character(char, i in replacements)
+    end)
+    |> Enum.join()
+  end
+
+  defp replace_character(_character, true) do
+    "_"
+  end
+
+  defp replace_character(character, false) do
+    character
+  end
+
+  def example, do: %__MODULE__{text: "abcde", schedule: [[1, 2, 3], [4, 5]]}
 end
