2fb30eeb1c20fa3bfc1c35d7c79df37de5f2dd5d
Paul McAvoy <paul.mcavoy@rsa.com>
2020-12-15 16:43:36 -0500

    Eraser constructor setup.

--- memotron/lib/memotron/eraser/core.ex
+++ memotron/lib/memotron/eraser/core.ex
@@ -1,13 +1,23 @@
-def Memotron.Eraser.Core do
+defmodule Memotron.Eraser.Core do
   defstruct [:schedule, :text]
-  
-  def new(text, steps) do
-    %__MODULE__{}
+
+  # constructor
+  def new(text, steps) when is_binary(text) and is_integer(steps) do
+    l = String.length(text)
+
+    schedule = Enum.shuffle(1..l) |> Enum.chunk_every(Kernel.ceil(l/steps))
+
+    %__MODULE__{text: text, schedule: schedule}
   end
-  
+
+  # converter
+  #def string_to_array(text) do
+  #end
+
+  # reducer
   def erase(eraser) do
     eraser
   end
-  
+
   def example, do: %{text: "abcde", schedule: [[1, 2, 3], [4, 5]]}
 end
