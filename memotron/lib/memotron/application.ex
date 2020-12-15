f9af81c348f7fe9a0f07356ae6d924d233c885e2
Raza <Ali.Raza1@rsa.com>
2020-12-16 01:14:23 +0530

    Fewer cats

---
+++ memotron/lib/memotron/application.ex
@@ -0,0 +1,34 @@
+defmodule Memotron.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Start the Ecto repository
+      Memotron.Repo,
+      # Start the Telemetry supervisor
+      MemotronWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: Memotron.PubSub},
+      # Start the Endpoint (http/https)
+      MemotronWeb.Endpoint
+      # Start a worker by calling: Memotron.Worker.start_link(arg)
+      # {Memotron.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Memotron.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    MemotronWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
---
+++ memotron/lib/memotron_web/live/counter_live.ex
@@ -0,0 +1,35 @@
+defmodule MemotronWeb.CounterLive do
+  use MemotronWeb, :live_view
+  alias Memotron.Core
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, count: Core.new(42))}
+  end
+
+  def render(assigns) do
+   ~L"""
+   <H1>You have <%= @count %> cats.</H1>
+   <button phx-click="increment">More Cats</button>
+   <button phx-click="decrement">Fewer Cats</button>
+   """
+  end
+
+  # Event Handlers
+  def handle_event("increment", _, socket) do
+    {:noreply, inc(socket)}
+  end
+
+  def handle_event("decrement", _, socket) do
+    {:noreply, dec(socket)}
+  end
+
+  # Private Functions
+  defp inc(socket) do
+    assign(socket, count: Core.increment(socket.assigns.count))
+  end
+
+  defp dec(socket) do
+    assign(socket, count: Core.decrement(socket.assigns.count))
+  end
+end
