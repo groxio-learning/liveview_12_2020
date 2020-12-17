6ce75e93574d9b23aafcc4b3321a0d2d2b1936ec
Paul McAvoy <paulmcav@queda.net>
2020-12-17 17:04:13 +0000

    Added counter component.

--- memotron/lib/memotron/library/passage.ex
+++ memotron/lib/memotron/library/passage.ex
@@ -15,5 +15,8 @@ defmodule Memotron.Library.Passage do
     passage
     |> cast(attrs, [:text, :steps, :name])
     |> validate_required([:text, :steps, :name])
+    |> validate_length(:name, min: 2)
+    |> validate_length(:text, min: 4)
+    |> validate_number(:steps, greater_than: 1, less_than: 10)
   end
 end
