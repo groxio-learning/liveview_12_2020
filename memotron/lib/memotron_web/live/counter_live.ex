71e50f0d4b39004c34c535e53ce93df97d67af9b
Raza <Ali.Raza1@rsa.com>
2020-12-17 20:29:54 +0530

    Up and Down

--- memotron/lib/memotron_web/live/counter_live.ex
+++ memotron/lib/memotron_web/live/counter_live.ex
@@ -7,19 +7,19 @@ defmodule MemotronWeb.CounterLive do
     {:ok, assign(socket, count: Core.new(42))}
   end
 
-  @impl true
-  def render(assigns) do
-   ~L"""
-   <H1>You have <%= @count %> cats.</H1>
-   <button phx-click="increment">More Cats</button>
-   <button phx-click="decrement">Fewer Cats</button>
-   """
-  end
-
   # Event Handlers
   @impl true
   def handle_event("fast_increment", _, socket) do
     {:noreply, socket |> inc() |> inc() }
+  end
+  @impl true
+  def handle_event("count", %{"value" => %{"direction" => "up"}},socket) do
+    {:noreply, socket |> inc() }
+  end
+
+  @impl true
+  def handle_event("count", %{"value" => %{"direction" => "down"}},socket) do
+    {:noreply, socket |> dec() }
   end
 
   @impl true
